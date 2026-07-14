7499d356e1b5c9b36a3e0ede20bdb0e279826ab0
Abdul Moeez <abdulmoeez@creationnext.com>
2026-07-14 18:06:28 +0500

    Update UseEffectUsingAsyncAwait.js

--- src/hooks/useEffect/UseEffectUsingAsyncAwait.js
+++ src/hooks/useEffect/UseEffectUsingAsyncAwait.js
@@ -27,40 +27,62 @@
 
 // export default UseEffectUsingAsyncAwait;
 
-// --- Handling Errors using Async/Await ---
-import { useEffect, useState } from "react";
+// // --- Handling Errors using Async/Await ---
+// import { useEffect, useState } from "react";
+
+// function UseEffectUsingAsyncAwait() {
+//   const [users, setUsers] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(
+//           "https://jsonplaceholder.typicode.com/users",
+//         );
+
+//         const data = await response.json();
+//         setUsers(data);
+//         console.log(data);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Users</h1>
+//       <ul>
+//         {users.map((user) => (
+//           <li key={user.id}>
+//             {user.id} - {user.name} - {user.address.zipcode}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default UseEffectUsingAsyncAwait;
+
+import React from "react";
+
+const products = [
+  { id: 1, name: "Laptop", price: 50000 },
+  { id: 2, name: "Mobile", price: 30000 },
+];
 
 function UseEffectUsingAsyncAwait() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users",
-        );
-
-        const data = await response.json();
-        setUsers(data);
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div>
-      <h1>Users</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.id} - {user.name} - {user.address.zipcode}
-          </li>
-        ))}
-      </ul>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <p>Price: Rs. {product.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
