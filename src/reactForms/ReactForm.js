7fdd6ae3cd617da672a05568dd0c0d9519fdad8a
Abdul Moeez <abdulmoeez@creationnext.com>
2026-07-17 17:47:35 +0500

    Added new code

--- src/reactForms/ReactForm.js
+++ src/reactForms/ReactForm.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ReactForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  // Event Handler
+  const handleSubmit = (e) => {
+    // prevent the page to reload on submt button
+    e.preventDefault();
+
+    // // user object
+    // const user = {
+    //   firstName,
+    //   lastName,
+    //   email,
+    // };
+
+    // Added user information to the local storage
+    // localStorage.setItem("user", JSON.stringify(user));
+
+    // const storedUser = JSON.parse(localStorage.getItem("user"));
+
+    // console.log("User is: ", storedUser);
+
+    console.log("First Name:", firstName);
+    console.log("Last Name:", lastName);
+    console.log("Email:", email);
+
+    // Reset fields
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+  };
+
   return (
-    <div className="mt-10 p-10">
+    <div className="max-w-5xl mx-auto mt-10 p-10">
       <h1 className="App text-2xl font-bold">React Form</h1>
-      <form className="space-y-5">
+      <form onSubmit={handleSubmit} className="space-y-5">
         {/* First Name */}
         <div>
           <label
@@ -14,12 +47,14 @@ function ReactForm() {
             First Name
           </label>
           <input
-            type="firstName"
+            type="text"
             id="firstName"
             name="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
             placeholder="Enter your first name"
             required
-            className="p-3 text-sm text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-none dark:text-slate-50 dark:bg-neutral-700 dark:outline-neutral-600"
+            className="p-3 text-sm text-slate-900 rounded-md bg-white w-full focus:outline-none dark:text-slate-50 dark:bg-neutral-700 dark:outline-neutral-600"
             autoComplete="off"
           />
         </div>
@@ -33,12 +68,14 @@ function ReactForm() {
             Last Name
           </label>
           <input
-            type="lastName"
+            type="text"
             id="lastName"
             name="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
             placeholder="Enter your last name"
             required
-            className="p-3 text-sm text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-none dark:text-slate-50 dark:bg-neutral-700 dark:outline-neutral-600"
+            className="p-3 text-sm text-slate-900 rounded-md bg-white w-full focus:outline-none dark:text-slate-50 dark:bg-neutral-700 dark:outline-neutral-600"
             autoComplete="off"
           />
         </div>
@@ -55,9 +92,11 @@ function ReactForm() {
             type="email"
             id="email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Enter your email here"
             required
-            className="p-3 text-sm text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-none dark:text-slate-50 dark:bg-neutral-700 dark:outline-neutral-600"
+            className="p-3 text-sm text-slate-900 rounded-md bg-white w-full focus:outline-none dark:text-slate-50 dark:bg-neutral-700 dark:outline-neutral-600"
             autoComplete="off"
           />
         </div>
